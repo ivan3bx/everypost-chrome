bd2abd45e32b1726a4d22d20415dae11db3a1682
ivan <moscoso@gmail.com>
2022-07-03 17:43:21 -0500

    extracts 'calculatePath'
    - highlights currently highlighted root
    - mark highlighted section with !important
    - keeps track of current highlight

--- src/entry/extract-links.ts
+++ src/entry/extract-links.ts
@@ -1,27 +1,51 @@
 console.log("extracting links...");
 
-window.addEventListener('mouseover',function(e) {
+
+let currentHighlight: Element
+
+window.addEventListener('mouseover', function (e) {
+    let path = Array<Element>()
 
     // is this part of a div that contains some content?
     if (e.target instanceof Element) {
-        var el = e.target
+        path = calculatePath(this.document, e.target)
+    }
 
-        while (el.tagName != "DIV") {
-            let parent = el.parentElement
-            if (parent instanceof Element) {
-                el = parent
-            } 
-            
+    if (path.length > 0 && path[0] instanceof Element) {
+        currentHighlight = path[0]
+        path[0].classList.add('highlighted');
+    }
+});
+
+window.addEventListener('mouseout', function (e) {
+    if (e.target instanceof Element) {
+        const path = calculatePath(this.document, e.target)
+        if (path[0] == currentHighlight) {
+            console.log("Removing")
+            currentHighlight.classList.remove("highlighted")
+        } else {
+            console.log("Not removing")
         }
     }
-
-    if (e.target instanceof Element && e.target.tagName == "DIV") {
-        e.target.classList.add('highlighted');
-    }
 });
 
-window.addEventListener('mouseout',function(e) {
-    if (e.target instanceof Element) {
-        e.target.classList.remove('highlighted');
+function calculatePath(document: Document, el?: Element): Array<Element> {
+    const path = [el as Element]
+
+    while (el != null) {
+        if (el.tagName == "DIV" && el.textContent != null && el.textContent.length > 0) {
+            console.log(`Breaking: ${path.map((element) => `[${element.tagName}] > `)}`)
+            break
+        } else if (el.parentElement && el.parentElement != document.documentElement) {
+            path.unshift(el.parentElement)
+            console.log(`Appended: ${path.map((element) => `[${element.tagName}] > `)}`)
+        } else {
+            console.log(`Else..: ${path.map((element) => `[${element.tagName}] > `)}`)
+            break
+        }
+
+        el = el.parentElement
     }
-});
+
+    return path
+}
