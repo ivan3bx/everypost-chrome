c5ea8eb8f940ca554b936762dc51f42f2824a86a
ivan <moscoso@gmail.com>
2022-07-03 13:53:49 -0500

    adds command shortcuts for link processing
    - ctrl-M to extract links
    - ctrl-shift-M to display popup

--- src/entry/background.ts
+++ src/entry/background.ts
@@ -1,1 +1,23 @@
+let active = false;
+
 console.log('hello world background todo something~')
+
+function makeOrange(color:string) {
+    console.log("making color: " + color)
+    document.body.style.backgroundColor = color
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    console.log("added listener..")
+    active = !active;
+    const color = active ? 'orange' : 'white'
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id ? tab.id : -1},
+        func: makeOrange,
+        args: [color]
+    }).then()
+})
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command invoked: ${command}'`)
+})
