79f2604e01f9a7c3abd1d8312cc3a4c52966fcfd
ivan <moscoso@gmail.com>
2022-08-27 14:22:16 -0500

    handle window/tab switching
    - adds logic for window activation
    - consolidates 'update badge' code
    - handles update of badge text consistently

--- src/entry/background.ts
+++ src/entry/background.ts
@@ -6,6 +6,24 @@ const linkMap = new LinkMapping(
     new Set(["localhost", "127.0.0.1", "mail.google.com"])
 )
 
+function updateWithTab(tab?: chrome.tabs.Tab) {
+    if (tab == null) {
+        console.warn("update passed a null tab instance ; ignoring")
+        return
+    }
+    const url = tab.url
+    const tabId = tab.id
+
+    if (url && url.length > 0) {
+        console.log("onFocusChanged: getting links and setting text for url:", url);
+        linkMap.getLinks(url, (links) => { setBadgeText(links.length, tabId) })
+    } else {
+        console.log("onFocusChanged: setting badge text to zero");
+        setBadgeText(0, tabId)
+    }
+
+}
+
 // Message dispatch
 chrome.runtime.onMessage.addListener((message, sender, callback) => {
     switch (message.action) {
@@ -16,9 +34,7 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
             break;
         case "check_url":
             console.debug("onMessage: checking URL")
-            linkMap.getLinks(message.url, (links) => {
-                setBadgeText(links.length)
-            })
+            updateWithTab(sender.tab)
             break
         default:
             console.debug("onMessage: unrecognized action", message.action)
@@ -28,27 +44,26 @@ chrome.runtime.onMessage.addListener((message, sender, callback) => {
 
 // Handle cases where sites push state via XHR (youtube, etc..)
 chrome.webNavigation.onHistoryStateUpdated.addListener((message) => {
-    console.debug("webNavigation detected history state change")
-    linkMap.getLinks(message.url, (links) => {
-        setBadgeText(links.length)
-    })
+    console.debug("webNavigation history state updated")
+    chrome.tabs.get(message.tabId, (tab) => { updateWithTab(tab) })
 })
 
-// WIP: change badge icon based on tab
+// Handle window activation events
+chrome.windows.onFocusChanged.addListener(windowId => {
+    if (windowId == chrome.windows.WINDOW_ID_NONE) {
+        // all windows lost focus ; no-op
+    } else {
+        chrome.tabs.query({ active: true, windowId: windowId }, (tabs) => {
+            updateWithTab(tabs[0])
+        })
+    }
+})
+
+// Handle tab activation events
 chrome.tabs.onActivated.addListener((info) => {
+    console.debug("Tab activated..")
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-        const url = tabs[0].url
-        if (url && url.length > 0) {
-            console.log("getting links and setting text for url:", url);
-
-            linkMap.getLinks(url, (links) => {
-                setBadgeText(links.length)
-            })
-        } else {
-            console.log("setting badge text to zero");
-
-            setBadgeText(0)
-        }
+        updateWithTab(tabs[0])
         console.log("Tab activated: id:", info.tabId, "windowID:", info.windowId, "URL:", tabs[0].url)
     })
 });
@@ -87,11 +102,11 @@ function updateStatus() {
 
 // Highlight a counter as badge text
 // eslint-disable-next-line
-function setBadgeText(count: number) {
+function setBadgeText(count: number, tabId?: number) {
     const tag = (count > 8) ? "9+" : `${count}`
-    chrome.action.setBadgeText({ text: (count > 0) ? tag : "" })
+    chrome.action.setBadgeText({ text: (count > 0) ? tag : "", tabId: tabId })
     chrome.action.setBadgeBackgroundColor({ color: '#e62e00' });
 }
 
 
-console.log('hello world background todo something~')
+console.log('EveryPost says hi!')
